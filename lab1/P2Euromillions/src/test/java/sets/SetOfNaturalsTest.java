0732b069cd459db6808dc14f25fe64615f66f55c
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-03-19 15:24:40 +0000

    Updated With Ex2 Lab01

---
+++ lab1/P2Euromillions/src/test/java/sets/SetOfNaturalsTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * @author ico0
+ */
+public class SetOfNaturalsTest {
+    private SetOfNaturals setA;
+    private SetOfNaturals setB;
+    private SetOfNaturals setC;
+    private SetOfNaturals setD;
+
+    @BeforeEach
+    public void setUp() {
+        setA = new SetOfNaturals();
+        setB = SetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
+
+        setC = new SetOfNaturals();
+        for (int i = 5; i < 50; i++) {
+            setC.add(i * 10);
+        }
+        setD = SetOfNaturals.fromArray(new int[]{30, 40, 50, 60, 10, 20});
+    }
+
+    @AfterEach
+    public void tearDown() {
+        setA = setB = setC = setD = null;
+    }
+
+    @Test
+    public void testAddElement() {
+        setA = new SetOfNaturals();
+        setB = SetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
+
+        setA.add(99);
+        assertTrue(setA.contains(99), "add: added element not found in set.");
+        assertEquals(1, setA.size());
+
+        setB.add(11);
+        assertTrue(setB.contains(11), "add: added element not found in set.");
+        assertEquals(7, setB.size(), "add: elements count not as expected.");
+    }
+
+    @Test
+    public void testAddNegNumber(){
+        setA = new SetOfNaturals();
+        assertThrows(IllegalArgumentException.class, ()->setA.add(-50));
+    }
+
+
+
+    @Test
+    public void testNoDuplicates() {
+        setB = SetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
+        assertThrows(IllegalArgumentException.class, ()->setB.add(10));
+    }
+
+    @Test
+    public void testAddBadArray() {
+        setA = new SetOfNaturals();
+        int[] elems = new int[]{10, 20, -30};
+
+        // must fail with exception
+        assertThrows(IllegalArgumentException.class, () -> setA.add(elems));
+    }
+
+
+    @Test
+    public void testIntersectForNoIntersection() {
+        setA = new SetOfNaturals();
+        setB = SetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
+        assertFalse(setA.intersects(setB), "no intersection but was reported as existing");
+
+    }
+
+
+}
